d3f34dfb1752adbc4777cb0171fa98a9922c5c7b
Paul S <landofalcon007@gmail.com>
2020-03-02 14:01:33 -0600

    Fizzbuzz program

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fizz = 0;
+            int buzz = 0;
+            int fizzbuzz = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    System.Console.WriteLine("Fizzbuzz"); // 66
+                    fizzbuzz ++;
+                }
+                else if (i % 3 == 0)
+                {
+                    System.Console.WriteLine("Fizz"); // 267
+                    fizz ++;
+                }
+                else if (i % 5 == 0)
+                {
+                    System.Console.WriteLine("Buzz"); // 134
+                    buzz ++;
+                }
+                else
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+            System.Console.WriteLine("The total amount of 'Fizzes' are : " + fizz);
+            System.Console.WriteLine("The total amount of 'Buzzes' are : " + buzz);
+            System.Console.WriteLine("The total amount of 'Fizzbuzzes' are : " + fizzbuzz);
+
+        }
+    }
+}
