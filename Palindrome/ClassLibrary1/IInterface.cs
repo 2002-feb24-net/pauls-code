3f211c45d91d60e5bcc9efbc3eea0d63b0907571
Paul S <landofalcon007@gmail.com>
2020-03-16 12:40:03 -0500

    Added Palindrome class and test

---
+++ Palindrome/ClassLibrary1/IInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+	interface IInterface
+	{
+		public void CheckPal() {}
+		
+	}
+}
---
+++ Palindrome/ClassLibrary1/Palindrome.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Palindrome : IInterface
+    {
+        public bool CheckPal(string inputString)
+        {
+
+            string first = inputString.Substring(0, inputString.Length / 2);
+            char[] arr = inputString.ToCharArray();
+
+            Array.Reverse(arr);
+
+            string temp = new string(arr);
+            string second = temp.Substring(0, temp.Length / 2);
+
+            return first.Equals(second, StringComparison.InvariantCultureIgnoreCase);
+
+        }
+    }
+}
